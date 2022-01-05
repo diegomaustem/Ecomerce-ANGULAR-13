0724597157fba1af3491b5d01b482a939437c1ea
diegomaustem <diego.maustem@gmail.com>
2022-01-05 16:54:58 -0300

    criando-componetes-da-pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeaderMainComponent } from './header/header-main/header-main.component';
+import { MainNavComponent } from './header/main-nav/main-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeaderMainComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
